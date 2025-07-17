136fd040772606b2ebece43743dc446359b3089b
chaxnsee <enchaxcreative@yahoo.com>
2025-07-17 00:26:01 -0700

    Update sketch.js

--- Paraverse/sketch.js
+++ Paraverse/sketch.js
@@ -29,7 +29,7 @@ function setup() {
 }
 
 function draw() {
-  background(0, 4);
+  background(0, 5);
  loadPixels(); // Call ONCE before update loop
 molds.forEach(m => m.update());
   textAlign(CENTER, CENTER);
