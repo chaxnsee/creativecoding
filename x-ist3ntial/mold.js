2404002e116a114e44947a6c0fd62171b2f2dc55
chaxnsee <enchaxcreative@yahoo.com>
2025-08-04 00:23:30 -0700

    Update mold.js

--- x-ist3ntial/mold.js
+++ x-ist3ntial/mold.js
@@ -115,7 +115,7 @@ display() {
     
    
     // Apply shadow (glow)
-  drawingContext.shadowBlur = 6.5;                      // Glow size
+  drawingContext.shadowBlur = 8.5;                      // Glow size
   drawingContext.shadowColor = currentColor;           // Glow color
 ellipse(this.x, this.y, this.r * 4, this.r * 4);
   // Reset shadow for next drawing calls (important!)
